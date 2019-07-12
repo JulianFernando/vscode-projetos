dfd6a503912acdd41ed36aac3a3f7824005035cd
JulianFernando <juliancambraia@gmail.com>
2019-07-11 22:02:16 -0300

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-galeria';
+  listaUsuarios = [
+    { id: 1, nome: 'Alexandre' },
+    { id: 2, nome: 'Ana' },
+    { id: 3, nome: 'Maria Inês' },
+    { id: 4, nome: 'Julian Fernando' },
+    { id: 5, nome: 'Frederico' },
+    { id: 6, nome: 'Eduardo' },
+  ]
 }
